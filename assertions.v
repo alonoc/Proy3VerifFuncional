95be2574cdf5f666313f5b1fd21eaf93e38e51f6
Edward Anthony Umana Willams <ESTUDIANTES\eumana@PoolCentOS607.estudiantes.ie.tec.ac.cr>
2018-12-06 02:53:13 -0600

    Added cas assert

--- assertions.v
+++ assertions.v
@@ -21,6 +21,10 @@ sequence inhibit_cmd;
 	wbox_intf.sdram_ras_n == 1'bx and wbox_intf.sdram_cas_n == 1'bx and wbox_intf.sdram_we_n == 1'bx;
 endsequence;
 
+sequence read_cmd;
+	wbox_intf.sdram_ras_n and !wbox_intf.sdram_cas_n and wbox_intf.sdram_we_n;
+endsequence
+
 sequence precharge_cmd;
 	!wbox_intf.sdram_ras_n and wbox_intf.sdram_cas_n and !wbox_intf.sdram_we_n;
 endsequence
@@ -49,12 +53,21 @@ property prop_wb_std_acknowledge;
 	@(posedge wbox_intf.wb_clk_i) $rose(wbox_intf.wb_stb_i) and $rose(wbox_intf.wb_cyc_i) |=> wbox_intf.wb_stb_i and wbox_intf.wb_cyc_i and wbox_intf.wb_ack_o;
 endproperty
 
+property prop_sdram_cas_wait;
+	@(posedge wbox_intf.sdram_clk) read_cmd ##[2:3] wbox_intf.sdram_cas |-> !$isunknown(wbox_intf.sdram_dq);
+endproperty
+
+property prop_sdram_cas_valid;
+	@(posedge wbox_intf.sdram_clk) wbox_intf.sdram_cas == 3'b010 or wbox_intf.sdram_cas == 3'b011;
+endproperty
+
 sdram_init		: assert property (prop_sdram_correct_init)			else $error("sdram initialization Failed");
 wb_rule_3_00 	: assert property (prop_wb_sigs_init_during_rst) 	else $error("wishbone rule 3.00 Failed");
 wb_rule_3_05 	: assert property (prop_rst_at_least_one_cycle) 	else $error("wishbone rule 3.05 Failed");
 wb_rule_3_25 	: assert property (prop_are_cyc_and_stb_equal) 		else $error("wishbone rule 3.25 Failed");
 wb_rule_3_35 	: assert property (prop_wb_std_acknowledge)			else $error("wishbone rule 3.35 Failed");
-
+prog_cas_rule	: assert property (prop_sdram_cas_wait)			else $error("Cas wait failed");
+prog_cas_valid	: assert property (prop_sdram_cas_valid)		else $error("Cas is invalid");
 endmodule;
 
 `endif
